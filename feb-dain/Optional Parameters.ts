825307d0cbcfe74f00c48156048cd683f3eb8bfe
Dain Lee <108778921+feb-dain@users.noreply.github.com>
2023-03-09 16:01:25 +0900

    solve: Optional Parameters Problem

---
+++ feb-dain/Optional Parameters.ts
@@ -0,0 +1,43 @@
+// problem
+import { expect, it } from 'vitest';
+
+export const getName = (first: string, last: string) => {
+  if (last) {
+    return `${first} ${last}`;
+  }
+  return first;
+};
+
+it('Should work with just the first name', () => {
+  const name = getName('Matt');
+
+  expect(name).toEqual('Matt');
+});
+
+it('Should work with the first and last name', () => {
+  const name = getName('Matt', 'Pocock');
+
+  expect(name).toEqual('Matt Pocock');
+});
+
+// solution
+import { expect, it } from 'vitest';
+
+export const getName = (first: string, last?: string) => {
+  if (last) {
+    return `${first} ${last}`;
+  }
+  return first;
+};
+
+it('Should work with just the first name', () => {
+  const name = getName('Matt');
+
+  expect(name).toEqual('Matt');
+});
+
+it('Should work with the first and last name', () => {
+  const name = getName('Matt', 'Pocock');
+
+  expect(name).toEqual('Matt Pocock');
+});
