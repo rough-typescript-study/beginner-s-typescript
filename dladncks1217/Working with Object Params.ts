4fc5002298c6599420946ae8459ad26a7c546cbf
임우찬 <dlaxodud1217@naver.com>
2023-03-09 15:48:58 +0900

    solve: Working with Object Params Problem

---
+++ dladncks1217/Working with Object Params.ts
@@ -0,0 +1,52 @@
+// problem
+import { expect, it } from "vitest";
+
+export const addTwoNumbers = (params) => {
+  return params.first + params.second;
+};
+
+it("Should add the two numbers together", () => {
+  expect(
+    addTwoNumbers({
+      first: 2,
+      second: 4,
+    }),
+  ).toEqual(6);
+
+  expect(
+    addTwoNumbers({
+      first: 10,
+      second: 20,
+    }),
+  ).toEqual(30);
+});
+
+
+
+// solution
+import { expect, it } from "vitest";
+
+type Check = {
+  first:number,
+  second:number
+};
+
+export const addTwoNumbers = (params:Check) => {
+  return params.first + params.second;
+};
+
+it("Should add the two numbers together", () => {
+  expect(
+    addTwoNumbers({
+      first: 2,
+      second: 4,
+    }),
+  ).toEqual(6);
+
+  expect(
+    addTwoNumbers({
+      first: 10,
+      second: 20,
+    }),
+  ).toEqual(30);
+});
