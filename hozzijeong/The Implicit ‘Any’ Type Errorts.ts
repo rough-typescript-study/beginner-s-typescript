70b7bc8346fc54d4598f677a490ec23ad084a63b
정호진 <50974359+hozzijeong@users.noreply.github.com>
2023-03-09 15:35:54 +0900

    solve: solve the 'any' type error problem
    (이 문제)[https://www.totaltypescript.com/tutorials/beginners-typescript/implicit-any-type-error] 해결

---
+++ hozzijeong/The Implicit ‘Any’ Type Errorts.ts
@@ -0,0 +1,23 @@
+// problem
+import { expect, it } from "vitest";
+
+export const addTwoNumbers = (a, b) => {
+  return a + b;
+};
+
+it("Should add the two numbers together", () => {
+  expect(addTwoNumbers(2, 4)).toEqual(6);
+  expect(addTwoNumbers(10, 10)).toEqual(20);
+});
+
+// solution
+import { expect, it } from "vitest";
+
+export const addTwoNumbers = (a:number, b:number):number => {
+  return a + b;
+};
+
+it("Should add the two numbers together", () => {
+  expect(addTwoNumbers(2, 4)).toEqual(6);
+  expect(addTwoNumbers(10, 10)).toEqual(20);
+});
