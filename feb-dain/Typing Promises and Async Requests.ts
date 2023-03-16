dae4a81eaa6c3da561155a9667c7ae33bb60c1ba
Dain Lee <108778921+feb-dain@users.noreply.github.com>
2023-03-16 20:22:57 +0900

    solve: Typing Promises and Async Requests Problem

---
+++ feb-dain/Typing Promises and Async Requests.ts
@@ -0,0 +1,48 @@
+// problem
+interface LukeSkywalker {
+  name: string;
+  height: string;
+  mass: string;
+  hair_color: string;
+  skin_color: string;
+  eye_color: string;
+  birth_year: string;
+  gender: string;
+}
+
+export const fetchLukeSkywalker = async (): LukeSkywalker => {
+  const data = await fetch("https://swapi.dev/api/people/1").then((res) => {
+    return res.json();
+  });
+
+  return data;
+};
+
+// solution
+interface LukeSkywalker {
+  name: string;
+  height: string;
+  mass: string;
+  hair_color: string;
+  skin_color: string;
+  eye_color: string;
+  birth_year: string;
+  gender: string;
+}
+
+export const fetchLukeSkywalker = async (): Promise<LukeSkywalker> => {
+  const data = await fetch('https://swapi.dev/api/people/1').then((res) => {
+    return res.json();
+  });
+
+  return data;
+};
+  
+// solution 2  
+export const fetchLukeSkywalker = async () => {
+  const data: LukeSkywalker = await fetch('https://swapi.dev/api/people/1').then((res) => {
+      return res.json();
+   });
+
+  return data;
+};
