504df2c37186de1939372e6675505bab35d939ad
박정규 <srk99279@naver.com>
2023-03-16 19:46:12 +0900

    solve: modified answer

--- regularpark/Assigning Types to Variables.ts
+++ regularpark/Assigning Types to Variables.ts
@@ -11,6 +11,8 @@ interface User {
  * How do we ensure that defaultUser is of type User
  * at THIS LINE - not further down in the code?
  */
+
+// using User
 const defaultUser: User = {
   id: 1,
   firstName: "hi",
