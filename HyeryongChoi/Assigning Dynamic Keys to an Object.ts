93d06d65d61596f4345e1fb10a9d767ea9c11c6e
최혜령 <chex1004@gmail.com>
2023-03-16 20:42:55 +0900

    solve: Assigning Dynamic Keys to an Object Problem

---
+++ HyeryongChoi/Assigning Dynamic Keys to an Object.ts
@@ -0,0 +1,118 @@
+// Problem
+import { expect, it } from "vitest";
+
+const createCache = () => {
+  const cache = {};
+
+  const add = (id: string, value: string) => {
+    cache[id] = value;
+  };
+
+  const remove = (id: string) => {
+    delete cache[id];
+  };
+
+  return {
+    cache,
+    add,
+    remove,
+  };
+};
+
+it("Should add values to the cache", () => {
+  const cache = createCache();
+
+  cache.add("123", "Matt");
+
+  expect(cache.cache["123"]).toEqual("Matt");
+});
+
+it("Should remove values from the cache", () => {
+  const cache = createCache();
+
+  cache.add("123", "Matt");
+  cache.remove("123");
+
+  expect(cache.cache["123"]).toEqual(undefined);
+});
+
+
+// Solution1
+
+import { expect, it } from "vitest";
+
+type Cache = {[key: string]: string} // {[index: string]: string}도 OK 
+
+const createCache = () => {
+  const cache: Cache = {};
+
+  const add = (id: string, value: string) => {
+    cache[id] = value;
+  };
+
+  const remove = (id: string) => {
+    delete cache[id];
+  };
+
+  return {
+    cache,
+    add,
+    remove,
+  };
+};
+
+it("Should add values to the cache", () => {
+  const cache = createCache();
+
+  cache.add("123", "Matt");
+
+  expect(cache.cache["123"]).toEqual("Matt");
+});
+
+it("Should remove values from the cache", () => {
+  const cache = createCache();
+
+  cache.add("123", "Matt");
+  cache.remove("123");
+
+  expect(cache.cache["123"]).toEqual(undefined);
+});
+
+// Solution2
+import { expect, it } from "vitest";
+
+const createCache = () => {
+  const cache: Record<string, string> = {};
+
+  const add = (id: string, value: string) => {
+    cache[id] = value;
+  };
+
+  const remove = (id: string) => {
+    delete cache[id];
+  };
+
+  return {
+    cache,
+    add,
+    remove,
+  };
+};
+
+it("Should add values to the cache", () => {
+  const cache = createCache();
+
+  cache.add("123", "Matt");
+
+  expect(cache.cache["123"]).toEqual("Matt");
+});
+
+it("Should remove values from the cache", () => {
+  const cache = createCache();
+
+  cache.add("123", "Matt");
+  cache.remove("123");
+
+  expect(cache.cache["123"]).toEqual(undefined);
+});
+
