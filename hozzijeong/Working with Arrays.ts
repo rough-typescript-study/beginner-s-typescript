5d32890cab1e7a39c1dd59ed24a16ea64e26c5c9
정호진 <50974359+hozzijeong@users.noreply.github.com>
2023-03-16 20:04:57 +0900

    refactor: add Generic way

--- hozzijeong/Working with Arrays.ts
+++ hozzijeong/Working with Arrays.ts
@@ -3,7 +3,7 @@ interface User {
   firstName: string;
   lastName: string;
   role: 'admin' | 'user' | 'super-admin';
-  posts: Post[];
+  posts: Post[]; // or Array<Post> 와 같이 바꾸기도 함. 사실 Array<T> 와 같은 방식이 Array와 관련된 내장 메서드 사용에 유용하기 때문에 이 방법을 권자함. 
 }
 
 interface Post {
