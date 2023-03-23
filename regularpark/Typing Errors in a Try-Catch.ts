6e2d09fcdf5a3ba8fea6ffcfa3b7e36c117d6486
박정규 <srk99279@naver.com>
2023-03-23 16:12:04 +0900

    solve: Typing Errors in a Try-Catch Problem

---
+++ regularpark/Typing Errors in a Try-Catch.ts
@@ -0,0 +1,35 @@
+# problem
+import { expect, it } from 'vitest';
+
+const tryCatchDemo = (state: 'fail' | 'succeed') => {
+  try {
+    if (state === 'fail') {
+      throw new Error('Failure!');
+    }
+  } catch (e) {
+    return e.message;
+  }
+};
+
+it('Should return the message when it fails', () => {
+  expect(tryCatchDemo('fail')).toEqual('Failure!');
+});
+
+
+
+# solve
+import { expect, it } from 'vitest';
+
+const tryCatchDemo = (state: 'fail' | 'succeed') => {
+  try {
+    if (state === 'fail') {
+      throw new Error('Failure!');
+    }
+  } catch (e) {
+    if (e instanceof Error) return e.message;
+  }
+};
+
+it('Should return the message when it fails', () => {
+  expect(tryCatchDemo('fail')).toEqual('Failure!');
+});
