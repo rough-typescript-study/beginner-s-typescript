b3c6db14e680ca85bf585326e5a9cee15e5c7f11
임우찬 <dlaxodud1217@naver.com>
2023-03-23 16:25:13 +0900

    solve: Combining Types to Create New Types Problem

---
+++ dladncks1217/Combining Types to Create New Types.ts
@@ -0,0 +1,36 @@
+interface User {
+  id: string;
+  firstName: string;
+  lastName: string;
+}
+
+interface Post {
+  id: string;
+  title: string;
+  body: string;
+}
+
+type UserAndPost = User & { posts: Post[] };
+
+/**
+ * How do we type this return statement so it's both
+ * User AND { posts: Post[] }
+ */
+export const getDefaultUserAndPosts = (): UserAndPost => {
+  return {
+    id: '1',
+    firstName: 'Matt',
+    lastName: 'Pocock',
+    posts: [
+      {
+        id: '1',
+        title: 'How I eat so much cheese',
+        body: "It's pretty edam difficult",
+      },
+    ],
+  };
+};
+
+const userAndPosts = getDefaultUserAndPosts();
+
+console.log(userAndPosts.posts[0]);
