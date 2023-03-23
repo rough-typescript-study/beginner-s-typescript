c556bc3243e316d92617b522067c6b560eb2e266
정호진 <50974359+hozzijeong@users.noreply.github.com>
2023-03-23 16:36:47 +0900

    feat: Selectively Construct Types from Other Types

---
+++ hozzijeong/Selectively Construct Types from Other Types.ts
@@ -0,0 +1,23 @@
+import { Equal, Expect } from './helpers/type-utils';
+
+interface User {
+  id: string;
+  firstName: string;
+  lastName: string;
+}
+
+/**
+ * How do we create a new object type with _only_ the
+ * firstName and lastName properties of User?
+ */
+
+/**
+ * type MyType = {
+ *   firstName: User['firstName'];
+ *   lastName: User['lastName'];
+ * };
+*/
+type MyType = Omit<User, 'id'>;
+
+
+type tests = [Expect<Equal<MyType, { firstName: string; lastName: string }>>];
