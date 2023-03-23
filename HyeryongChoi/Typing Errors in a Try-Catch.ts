66a9315d37b3c390aba3ecbdbc259d798936e7fb
최혜령 <chex1004@gmail.com>
2023-03-23 16:12:21 +0900

    solve: Typing Errors in a Try-Catch Problem

---
+++ HyeryongChoi/Typing Errors in a Try-Catch.ts
@@ -0,0 +1,33 @@
+// Problem
+import { expect, it } from "vitest";
+
+const tryCatchDemo = (state: "fail" | "succeed") => {
+  try {
+    if (state === "fail") {
+      throw new Error("Failure!");
+    }
+  } catch (e) {
+    return e.message;
+  }
+};
+
+it("Should return the message when it fails", () => {
+  expect(tryCatchDemo("fail")).toEqual("Failure!");
+});
+
+// Solution
+import { expect, it } from "vitest";
+
+const tryCatchDemo = (state: "fail" | "succeed") => {
+  try {
+    if (state === "fail") {
+      throw new Error("Failure!");
+    }
+  } catch (e) {
+    if (e instanceof Error) return e.message;
+  }
+};
+
+it("Should return the message when it fails", () => {
+  expect(tryCatchDemo("fail")).toEqual("Failure!");
+});
