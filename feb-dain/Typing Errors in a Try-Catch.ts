b446096c9d21f4323f0ee17ba3ddcaecd1397772
Dain Lee <108778921+feb-dain@users.noreply.github.com>
2023-03-23 16:11:33 +0900

    solve: Typing Errors in a Try-Catch Problem

---
+++ feb-dain/Typing Errors in a Try-Catch.ts
@@ -0,0 +1,34 @@
+// problem
+import { expect, it } from "vitest";
+
+const tryCatchDemo = (state: "fail" | "succeed") => {
+  try {
+    if (state === "fail") {
+      throw new Error("Failure!");
+    }
+  } catch (e) {
+    return e.message;
+  }
+};
+
+it("Should return the message when it fails", () => {
+  expect(tryCatchDemo("fail")).toEqual("Failure!");
+});
+
+// solution
+import { expect, it } from 'vitest';
+
+const tryCatchDemo = (state: 'fail' | 'succeed') => {
+  try {
+    if (state === 'fail') {
+      throw new Error('Failure!');
+    }
+  } catch (e) {
+    if (e instanceof Error) return e.message;
+  }
+};
+
+it('Should return the message when it fails', () => {
+  expect(tryCatchDemo('fail')).toEqual('Failure!');
+});
+
