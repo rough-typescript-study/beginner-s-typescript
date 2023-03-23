f9c32342eaa96e54d4b594c92a0947480246f04e
Dain Lee <108778921+feb-dain@users.noreply.github.com>
2023-03-23 16:17:27 +0900

    solve: Inheriting Interface Properties Problem

---
+++ feb-dain/Inheriting Interface Properties.ts
@@ -0,0 +1,60 @@
+// problem
+import { Equal, Expect } from "./helpers/type-utils";
+
+/**
+ * Here, the id property is shared between all three
+ * interfaces. Can you find a way to refactor this to
+ * make it more DRY?
+ */
+
+interface User {
+  id: string;
+  firstName: string;
+  lastName: string;
+}
+
+interface Post {
+  id: string;
+  title: string;
+  body: string;
+}
+
+interface Comment {
+  id: string;
+  comment: string;
+}
+
+type tests = [
+  Expect<Equal<User, { id: string; firstName: string; lastName: string }>>,
+  Expect<Equal<Post, { id: string; title: string; body: string }>>,
+  Expect<Equal<Comment, { id: string; comment: string }>>,
+];
+
+// solution
+ * interfaces. Can you find a way to refactor this to
+ * make it more DRY?
+ */
+
+interface Id {
+  id: string;
+}
+
+interface User extends Id {
+  firstName: string;
+  lastName: string;
+}
+
+interface Post extends Id {
+  title: string;
+  body: string;
+}
+
+interface Comment extends Id {
+  comment: string;
+}
+
+type tests = [
+  Expect<Equal<User, { id: string; firstName: string; lastName: string }>>,
+  Expect<Equal<Post, { id: string; title: string; body: string }>>,
+  Expect<Equal<Comment, { id: string; comment: string }>>
+];
