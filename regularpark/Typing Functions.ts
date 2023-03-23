863bb4dcd6c23f898396eb9a062105618b479dfe
박정규 <srk99279@naver.com>
2023-03-23 16:42:39 +0900

    solve: Typing Functions Problem

---
+++ regularpark/Typing Functions.ts
@@ -0,0 +1,48 @@
+import { Equal, Expect } from './helpers/type-utils';
+
+/**
+ * How do we type onFocusChange?
+ */
+const addListener = (onFocusChange: (isFocused: boolean) => void) => {
+  window.addEventListener('focus', () => {
+    onFocusChange(true);
+  });
+
+  window.addEventListener('blur', () => {
+    onFocusChange(false);
+  });
+};
+
+addListener((isFocused) => {
+  console.log({ isFocused });
+
+  type tests = [Expect<Equal<typeof isFocused, boolean>>];
+});
+
+
+
+#2
+
+import { Equal, Expect } from './helpers/type-utils';
+
+/**
+ * How do we type onFocusChange?
+ */
+
+type FocusListener = (isFocused: boolean) => void;
+
+const addListener = (onFocusChange: FocusListener) => {
+  window.addEventListener('focus', () => {
+    onFocusChange(true);
+  });
+
+  window.addEventListener('blur', () => {
+    onFocusChange(false);
+  });
+};
+
+addListener((isFocused) => {
+  console.log({ isFocused });
+
+  type tests = [Expect<Equal<typeof isFocused, boolean>>];
+});
